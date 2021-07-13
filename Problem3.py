0cd74b6163e72222295d3fb31288939bf810192b
denny190 <79014994+denny190@users.noreply.github.com>
2021-07-13 15:53:02 +0200

    Update Problem3.py

--- Problem3.py
+++ Problem3.py
@@ -1,3 +1,6 @@
+#The prime factors of 13195 are 5, 7, 13 and 29.
+#What is the largest prime factor of the number 600851475143 ?
+
 x = int(input())
 while x % 2 == 0:
     print(2)
@@ -10,3 +13,6 @@ for y in range(3, int(x**(1/2)) + 1, 2):
 
 if x > 2:
     print(int(x))
+
+#Basic algorithm for prime factorisation that I made as one of our many homeworks for Programming 1 and Algorithmisation at MFFUK. 
+#I just picked out the largest number it outputted. You could write it such as it only outputs the largest prime but I couldn't be bothered.
