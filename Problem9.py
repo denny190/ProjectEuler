3cf972ab6f06d22db19dcba7fba5f6e3a62020c2
denny190 <79014994+denny190@users.noreply.github.com>
2021-07-19 19:55:38 +0200

    Create Problem9.py

---
+++ Problem9.py
@@ -0,0 +1,16 @@
+#A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+#a^2 + b^2 = c^2
+#For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+#There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+#Find the product abc.
+
+for a in range(1,1000):
+    for b in range(1,1000):
+        c = 1000 - a - b
+        if (a**2 + b**2) == c**2:
+            print(a*b*c)
+          
+#Gotta say this problem was slightly less interesting than I expected it to be. It just goes down to iterating through the numbers again. 
+#I did find some time-saving shortcuts at a blog called RadiusOfCircle, generating number c with the condition c = 1000-a-b. 
+#Other than that, pretty straightforward
